5f73098707acf03ff4044e9ae911a8d5f4a1c820
Jaber <jaberriyan357@gmail.com>
2024-10-03 21:27:57 +0600

    all done

--- scripts/video.js
+++ scripts/video.js
@@ -11,6 +11,14 @@ const convertTimeHMS = (seconds) => {
 
 // convert second to hours,minutes, second function end 
 
+// remove all active class function start 
+const removeActiveClass = () => {
+    const btnContainer = document.querySelectorAll(".category-btn");
+    for (const btn of btnContainer) {
+        btn.classList.remove("active");
+    }
+}
+// remove all active class function end 
 
 // category wise loading videos start 
 const categoryWiseLoadData = async (category_id) => {
@@ -21,12 +29,11 @@ const categoryWiseLoadData = async (category_id) => {
     }
     const res = await fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${category_id}`);
     const data = await res.json();
-    // displayVideos(data);
-    // if (data.category.length === 0) {
-    //     noContentError();
-    //     return
-    // }
+    // remove active class from all category btn 
     displayVideos(data.category);
+    removeActiveClass();
+    const activeBtn = document.querySelector(`#btn-${category_id}`);
+    activeBtn.classList.add("active");
 }
 // category wise loading videos end 
 
@@ -51,6 +58,7 @@ const loadCategories = async () => {
 }
 // loadCategories end 
 
+
 // displayCategories start 
 const displayCategories = (categories) => {
     const categoryContainer = document.querySelector("#categories");
@@ -58,7 +66,7 @@ const displayCategories = (categories) => {
         // console.log(category.category);
         const buttonContainer = document.createElement('div');
         buttonContainer.innerHTML = `
-            <button onclick='categoryWiseLoadData(${category.category_id})' class="btn text-[#252525B3] font-[600] text-[1rem]">${category.category}</button>
+            <button id="btn-${category.category_id}" onclick='categoryWiseLoadData(${category.category_id})' class="btn font-[600] text-[1rem] category-btn">${category.category}</button>
         `
         // button.className = "btn text-[#252525B3] font-[600] text-[1rem]";
         // button.innerText = category.category;
@@ -70,9 +78,9 @@ const displayCategories = (categories) => {
 
 // load videos and display start 
 
-const loadVideos = async () => {
+const loadVideos = async (title = "") => {
     try {
-        const res = await fetch(`https://openapi.programming-hero.com/api/phero-tube/videos`);
+        const res = await fetch(`https://openapi.programming-hero.com/api/phero-tube/videos?title=${title}`);
         if (!res.ok) {
             throw new Error(`HTTP error! status: ${res.status}`);
         }
@@ -83,24 +91,52 @@ const loadVideos = async () => {
         console.log(err);
     }
 }
-const demoOjb = {
-    "category_id": "1001",
-    "video_id": "aaaa",
-    "thumbnail": "https://i.ibb.co/L1b6xSq/shape.jpg",
-    "title": "Shape of You",
-    "authors": [
-        {
-            "profile_picture": "https://i.ibb.co/D9wWRM6/olivia.jpg",
-            "profile_name": "Olivia Mitchell",
-            "verified": ""
-        }
-    ],
-    "others": {
-        "views": "100K",
-        "posted_date": "16278"
-    },
-    "description": "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
+// load details function start 
+const loadDetails = async (videoId) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phero-tube/video/${videoId}`);
+    const data = await res.json();
+    displayDetails(data.video);
 }
+// load details function end 
+
+// display details video function start
+const displayDetails = (video) => {
+    // console.log(video);
+    const detailsContainer = document.querySelector("#modal-content");
+    detailsContainer.innerHTML = `
+        <img class="h-[70%] w-full rounded-t-lg object-cover" src="${video.thumbnail}" alt="" />
+        <div class="px-0 py-3 flex gap-5 mb-3">
+            <div>
+                <img class="rounded-[50%] w-24 h-24 object-cover" src="${video?.authors[0]?.profile_picture}" alt="">
+            </div>
+            <div>
+                <h2 class=" font-[700] text-[#171717] text-2xl">${video.title}</h2>
+                <div class="flex gap-2 items-center">
+                    <p class="text-[#171717B3] font-[500] text-xl">${video?.authors[0]?.profile_name} </p>
+                    ${video?.authors[0]?.verified
+            ? `<img class="w-5 h-5" src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png" alt=''>`
+            : ""}
+                      
+                </div>
+                <p class="text-[#171717B3] font-[500] mb-2 text-xl">${video?.others?.views}</p>
+            </div>
+        </div>
+        <div>
+            <h3 class="text-xl font-[700] mb-2">Description : </h3>
+            <p>${video.description}</p>
+        </div>
+    `
+
+    // calling modal ways 
+    // way-1 
+    // document.querySelector('#showModalBtn').click();
+    // way-2 
+    // detailModal.showModal();
+    // way-3 
+    document.querySelector('#detailModal').showModal();
+}
+// display details video function end 
+
 const displayVideos = (videos) => {
     document.querySelector("#videos").innerHTML = "";
     const videoContainer = document.querySelector("#videos");
@@ -143,7 +179,10 @@ const displayVideos = (videos) => {
                 : ""}
                       
                 </div>
-                <p class="text-[#171717B3] font-[500]">${video?.others?.views}</p>
+                <p class="text-[#171717B3] font-[500] mb-2">${video?.others?.views}</p>
+                <div>
+                    <p><button onclick="loadDetails('${video.video_id}')" class="btn btn-accent">Details</button></p>
+                </div>
             </div>
         </div>
         `
@@ -151,6 +190,16 @@ const displayVideos = (videos) => {
     })
 }
 // load videos and display end 
+
+// search by keyword function start 
+const searchInput = document.querySelector("#searchInput");
+searchInput.addEventListener('keyup', (e) => {
+    loadVideos(e.target.value);
+    // console.log(e.target.value);
+    // loadVideos(searchInput.value);
+})
+// search by keyword function end
+
 // calling loadCategories function 
 loadCategories();
 // calling loadVideos function
